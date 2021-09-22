478a2aff11efa678e75e133dfbad5aab9be1e36e
Amey <ameydeshingkar@gmail.com>
2021-09-22 19:19:51 +0530

    Stateless to Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,39 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What is your favourite colour?",
+      "What is you favourite animal?",
+    ];
+
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('First App'),
       ),
       body: Column(children: <Widget>[
-        Text('Questions'),
-        RaisedButton(child: Text('Answer 1'), onPressed: null),
-        RaisedButton(child: Text('Answer 2'), onPressed: null),
-        RaisedButton(child: Text('Answer 3'), onPressed: null),
+        Text(questions[questionIndex]),
+        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+        RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+        RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
       ]),
     ));
   }
