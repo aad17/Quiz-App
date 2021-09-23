289a01a5f522cd2fa35f1c0ad651bfb212a561db
Amey <ameydeshingkar@gmail.com>
2021-09-23 18:21:47 +0530

    Questions And Answers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,37 +9,53 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
-  void answerQuestion() {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+      _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      "What is your favourite colour?",
-      "What is you favourite animal?",
+    const questions = [
+      {
+        'questionText': "What is your favourite colour?",
+        'answers': ['Black', 'Red', 'Green', 'White']
+      },
+      {
+        'questionText': "What is you favourite animal?",
+        'answers': ['Elephant', 'Rabbit', 'Lion', 'Tiger']
+      },
+      {
+        'questionText': "Whon is your favourite instructor?",
+        'answers': ['ABC', 'DEF', 'GHI', 'JKL']
+      },
     ];
 
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('First App'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: [
+            Question(
+              questions[_questionIndex]['questionText'],
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
+          ],
+        ),
       ),
-      body: Column(children: <Widget>[
-        Text(questions[questionIndex]),
-        RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
-        RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
-        RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
-      ]),
-    ));
+    );
   }
 }
