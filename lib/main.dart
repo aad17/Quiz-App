5c2b5a2cc7d7aff6e3297e6868ac0034f2990e1c
Amey <ameydeshingkar@gmail.com>
2021-09-23 20:05:34 +0530

    Complete App

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,44 @@ class _MyAppState extends State<MyApp> {
   final _questions = const [
     {
       'questionText': "What is your favourite colour?",
-      'answers': ['Black', 'Red', 'Green', 'White']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 6},
+        {'text': 'Green', 'score': 4},
+        {'text': 'White', 'score': 1}
+      ]
     },
     {
       'questionText': "What is you favourite animal?",
-      'answers': ['Elephant', 'Rabbit', 'Lion', 'Tiger']
+      'answers': [
+        {'text': 'Elephant', 'score': 7},
+        {'text': 'Rabbit', 'score': 0},
+        {'text': 'Lion', 'score': 10},
+        {'text': 'Tiger', 'score': 9}
+      ]
     },
     {
       'questionText': "Whon is your favourite instructor?",
-      'answers': ['ABC', 'DEF', 'GHI', 'JKL']
+      'answers': [
+        {'text': 'ABC', 'score': 7},
+        {'text': 'DEF', 'score': 0},
+        {'text': 'GHI', 'score': 10},
+        {'text': 'JKL', 'score': 9}
+      ]
     },
   ];
   var _questionIndex = 0;
-  void _answerQuestion() {
+  var _totalScore = 0;
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     // if (_questionIndex < questions.length) {}
     setState(() {
       _questionIndex = _questionIndex + 1;
@@ -50,7 +75,7 @@ class _MyAppState extends State<MyApp> {
               questionIndex: _questionIndex,
               questions: _questions,
             )
-          : Result(),
+          : Result(_totalScore, _resetQuiz),
     ));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -3,10 +3,43 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   // const result({Key? key}) : super(key: key);
 
+  final int resultScore;
+  final Function resetHandlar;
+
+  Result(this.resultScore, this.resetHandlar);
+
+  String get resultPhrase {
+    var resultText = 'You did it!!';
+    if (resultScore <= 8) {
+      resultText = 'You are GOOD';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likable';
+    } else if (resultScore <= 16) {
+      resultText = 'Pretty Strange';
+    } else {
+      resultText = 'Bad Boy';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You Did It!!'),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            onPressed: resetHandlar,
+            child: Text('Restart Quiz'),
+            textColor: Colors.blue[400],
+          )
+        ],
+      ),
     );
   }
 }
